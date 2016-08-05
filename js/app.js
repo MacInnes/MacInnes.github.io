9e2d6d7b66082d3a94b19d68773903ec2d216d92
Andrew Mockett <califormula@gmail.com>
2016-08-05 10:49:42 -0700

    merge commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log('JS FILE LOADED');
+var header = document.getElementById("headerWrap");
+
+header.addEventListener('mouseover', function(){
+  
+  console.log("event listener");
+})
+
+
+
+
+console.log(header);
